b3a39c765c66beaab31556b26d355fa2858552c6
Jhonnier <jhonnier.hernandez@correounivalle.edu.co>
2023-04-28 15:09:40 -0500

    Dejando esta app lista para produccion

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
-import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  constructor(private dbzService: DbzService) {}
+  constructor() {}
 
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
